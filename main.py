3f6ea8d8bb061d3d630a67bf6a8ed780645d992c
CRA2001 <CRA2001>
2026-07-14 16:43:05 +0300

    Added Labels and buttons

--- main.py
+++ main.py
@@ -8,4 +8,27 @@ Author: @Carlos Raniel Ariate Arro
 import customtkinter as ctk
 app = ctk.CTk()
 app.geometry("500x500")
+#Title section
+title = ctk.CTkLabel(
+    app,
+    text = "Run club project"
+)
+
+newEntryBtn = ctk.CTkButton(
+    app,
+    text = "Enter new record"
+)
+
+viewRecordsBtn = ctk.CTkButton(
+    app,
+    text = " View records"
+)
+exitBtn = ctk.CTkButton(
+    app,
+    text ="Exit"
+)
+title.pack(pady=30)
+newEntryBtn.pack(pady=20)
+viewRecordsBtn.pack(pady=20)
+exitBtn.pack(pady=20)
 app.mainloop()
