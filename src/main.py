e0fac0e3c9d11f73997643c9a3a8d6aa7ae08dff
CRA2001 <CRA2001>
2026-07-18 17:41:16 +0300

    Front end of add new records completed

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ Author: @Carlos Raniel Ariate Arro
 
 
 import customtkinter as ctk
+import sys
 app = ctk.CTk()
 app.geometry("500x500")
 #Title section
@@ -16,7 +17,11 @@ records= []
 def showPage(page):
     page.tkraise()
 
+def submitRecord():
+    pass
 
+def exit():
+    sys.exit()
 #--------------------------create container--------------------------
 container = ctk.CTkFrame(app)
 container.pack(fill="both",expand=True)
@@ -38,11 +43,36 @@ newEntryBtn = ctk.CTkButton(
 )
 newEntryBtn.pack(pady=20)
 addPage = ctk.CTkFrame(container)
+returnBtn = ctk.CTkButton(
+    addPage,
+    text="Return",
+    command = lambda:showPage(mainPage)
+)
+returnBtn.grid(row=0,column=0,pady=30,padx=20)
+
 addTitle = ctk.CTkLabel(
     addPage,
     text="Add new record"
 )
-addTitle.pack(pady=30)
+addTitle.grid(row=0,column=1,pady=30,padx=20)
+nameEntry = ctk.CTkEntry(
+    addPage,
+    placeholder_text= "Enter name "   
+)
+nameEntry.grid(row=1,column=1,pady=30,padx=20)
+timeEntry = ctk.CTkEntry(
+    addPage,
+    placeholder_text="time in seconds"
+)
+timeEntry.grid(row=4,column=1,pady=30,padx=20)
+
+submitbtn = ctk.CTkButton(
+    addPage,
+    text="Submit",
+    command=submitRecord
+)
+submitbtn.grid(row=5,column=1,pady=30,padx=20)
+
 #--------------------------View Records--------------------------
 viewRecordsBtn = ctk.CTkButton(
     mainPage,
@@ -61,6 +91,7 @@ viewRecordsTitle.pack(pady=30)
 exitBtn = ctk.CTkButton(
     mainPage,
     text ="Exit",
+    command = exit
 )
 exitBtn.pack(pady=20)
 
