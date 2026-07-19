6187f31a8cb83e19b6c1181bcc92101cebb9a87b
Carlos Arro <119028008+CRA2001@users.noreply.github.com>
2026-07-19 20:25:16 +0300

    Added the function getInput

---
+++ main.py
@@ -0,0 +1,113 @@
+'''
+Run club project
+Author: @Carlos Raniel Ariate Arro
+
+'''
+
+
+import customtkinter as ctk
+import sys
+app = ctk.CTk()
+app.geometry("500x500")
+#Title section
+
+# dataset
+memberTime= []
+memberName= []
+
+def getInput():
+    name = nameEntry.get()
+    time = timeEntry.get()
+
+    memberName.append(name)
+    memberTime.append(time)
+    print("Added new values: ", [name,time])
+    print("Updated arrays: ")
+    print(memberName)
+    print(memberTime)
+
+def showPage(page):
+    page.tkraise()
+
+def submitRecord():
+    pass
+
+def exit():
+    sys.exit()
+#--------------------------create container--------------------------
+container = ctk.CTkFrame(app)
+container.pack(fill="both",expand=True)
+container.grid_rowconfigure(0,weight=1)
+container.grid_columnconfigure(0,weight=1)
+#--------------------------Main Page--------------------------
+mainPage = ctk.CTkFrame(container)
+title = ctk.CTkLabel(
+    mainPage,
+    text = "Run club project"
+)
+title.pack(pady=30)
+
+#--------------------------newEntryBtn--------------------------
+newEntryBtn = ctk.CTkButton(
+    mainPage,
+    text = "Enter new record",
+    command = lambda: showPage(addPage)
+)
+newEntryBtn.pack(pady=20)
+addPage = ctk.CTkFrame(container)
+returnBtn = ctk.CTkButton(
+    addPage,
+    text="Return",
+    command = lambda:showPage(mainPage)
+)
+returnBtn.grid(row=0,column=0,pady=30,padx=20)
+
+addTitle = ctk.CTkLabel(
+    addPage,
+    text="Add new record"
+)
+addTitle.grid(row=0,column=1,pady=30,padx=20)
+nameEntry = ctk.CTkEntry(
+    addPage,
+    placeholder_text= "Enter name "
+)
+nameEntry.grid(row=1,column=1,pady=30,padx=20)
+timeEntry = ctk.CTkEntry(
+    addPage,
+    placeholder_text="time in seconds"
+)
+timeEntry.grid(row=4,column=1,pady=30,padx=20)
+
+submitbtn = ctk.CTkButton(
+    addPage,
+    text="Submit",
+    command=getInput
+)
+submitbtn.grid(row=5,column=1,pady=30,padx=20)
+
+#--------------------------View Records--------------------------
+viewRecordsBtn = ctk.CTkButton(
+    mainPage,
+    text = " View records",
+    command = lambda: showPage(viewRecords)
+)
+viewRecordsBtn.pack(pady=20)
+viewRecords = ctk.CTkFrame(container)
+viewRecordsTitle = ctk.CTkLabel(
+    viewRecords,
+    text="View Records"
+)
+viewRecordsTitle.pack(pady=30)
+
+#--------------------------Exit button--------------------------
+exitBtn = ctk.CTkButton(
+    mainPage,
+    text ="Exit",
+    command = exit
+)
+exitBtn.pack(pady=20)
+
+for page in (mainPage,addPage,viewRecords):
+    page.grid(row=0,column=0,sticky="nsew")
+mainPage.tkraise()
+app.mainloop()
